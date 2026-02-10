27282822c8a3f5ab80275b3d51532bd2c55e196a
Dhruvish <php.dhruvish@spaculus.info>
2026-02-10 16:32:09 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import'./Header.css'
+
+const Header = () => {
+return (
+    <div className='header'>
+        <div className='header-contents'>
+        <h2> Order Your Favourite Food Here </h2>
+        <p> We have a wide variety of food items to choose from. </p>
+        <button>View Menu</button>
+        </div>    
+    
+    </div>
+)
+}
+
+export default Header
